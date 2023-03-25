e30de02b26ff19222afe98e8df3d31451bb83997
Till Klink <till.klink@icloud.com>
2023-03-25 15:02:37 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const typed = new Typed(".multiple-text", {
+  strings: ["Klima Technik", "Kälte Technik", "Lüftungs Technik"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  backDelay: 1300,
+  loop: true
+});
